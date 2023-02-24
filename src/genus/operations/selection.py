d1599e7959f2e3c654c4f58f6fc22481543e61e1
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-24 15:57:24 -0300

    Added a docstring

--- src/genus/operations/selection.py
+++ src/genus/operations/selection.py
@@ -23,6 +23,22 @@ class ElitismSelection(Operation):
         amount: int = None,
         proportion: float = None,
     ) -> None:
+        """Create a configuration for selection using elitism.
+
+        Parameters
+        ----------
+        fitness : Callable[[Chromosome], float]
+            Fitness function that takes a chromosome and scores how strong
+            it is. A higher value means better offspring.
+        amount : int, optional
+            Amount of chromosomes to choose as the best, by default None. If
+            specified, it takes precedence over whatever value is indicated
+            by `proportion`.
+        proportion : float, optional
+            Proportion of the total size of chromosomes to choose as best,
+            by default None. If both `amount` and `proportion` are None,
+            it takes all the values.
+        """
         super().__init__()
         self.fitness = fitness
         self.amount = amount
