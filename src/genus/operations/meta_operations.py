c86bd9cca96690881bc3551368a30f9a3f37cadb
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-26 18:32:24 -0300

    Added an update function to Parallel

--- src/genus/operations/meta_operations.py
+++ src/genus/operations/meta_operations.py
@@ -36,12 +36,20 @@ class Sequential(Operation):
 class Parallel(Operation):
     """Apply multiple operations on parallel"""
 
-    def __init__(self, *operations: Operation) -> None:
+    def __init__(
+        self,
+        *operations: Operation,
+        _update_function: Callable[[object, Operation], object] = lambda x, op: op(x),
+    ) -> None:
         super().__init__()
         self.operations = operations
+        self._update_function = _update_function
 
     def forward(self, x: object) -> List:
         # Use multithreading to speed up the execution
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(op, x) for op in self.operations]
+            # futures = [executor.submit(op, x) for op in self.operations]
+            futures = [
+                executor.submit(self._update_function, x, op) for op in self.operations
+            ]
             return [f.result() for f in futures]
