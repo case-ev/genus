b75bc410ddd1e7bd8b49d1c71b2166458317309b
Cristobal Allendes <callendes.molina@gmail.com>
2023-03-17 11:52:48 -0300

    Added another class for parallel operations that preserve ordering

--- src/genus/ops/parallel.py
+++ src/genus/ops/parallel.py
@@ -11,7 +11,8 @@ from genus.ops.operation import Operation
 
 
 class Parallel(Operation):
-    """Apply multiple operations on parallel"""
+    """Apply multiple operations in parallel, without guaranteeing that
+    the results are in the same order as the inputs"""
 
     def __init__(
         self,
@@ -25,9 +26,28 @@ class Parallel(Operation):
         self.workers = workers
 
     def forward(self, x: object) -> List:
-        # Use multithreading to speed up the execution
         with concurrent.futures.ThreadPoolExecutor(self.workers) as executor:
-            return executor.map(
-                lambda arg: self._update_function(*arg),
-                ((x, op) for op in self.operations),
-            )
+            return executor.map(lambda arg: self._update_function(*arg), ((x, op) for op in self.operations))
+
+
+class ParallelOrdered(Operation):
+    """Apply multiple operations on parallel, guaranteeing that the order
+    of operations is preserved"""
+
+    def __init__(
+        self,
+        *operations: Operation,
+        _update_function: Callable[[object, Operation], object] = lambda x, op: op(x),
+        workers: int = None,
+    ) -> None:
+        super().__init__()
+        self.operations = operations
+        self._update_function = _update_function
+        self.workers = workers
+
+    def forward(self, x: object) -> List:
+        with concurrent.futures.ThreadPoolExecutor(self.workers) as executor:
+            futures = [
+                executor.submit(self._update_function, x, op) for op in self.operations
+            ]
+            return [f.result() for f in futures]
